5af7f507e24e6ac190057647d974dcc07162a214
nddat1811 <63857350+nddat1811@users.noreply.github.com>
2023-11-20 17:05:37 +0800

    add data sample

--- db/init.sql
+++ db/init.sql
@@ -27,12 +27,10 @@ CREATE TABLE `product` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` BOOLEAN,
   `tag` VARCHAR(255) NOT NULL,
-  `origin` INT,
+  `origin` VARCHAR(255),
   `category_id` INT,
-  `review_id` INT,
   `inventory_id` INT,
-  `discount_id` INT,
-  `price_id` INT
+  `discount_id` INT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -50,6 +48,7 @@ CREATE TABLE `product_category` (
 DROP TABLE IF EXISTS `product_price`;
 CREATE TABLE `product_price` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `product_id` INT,
   `price` FLOAT,
   `description` VARCHAR(255)
 );
@@ -78,6 +77,7 @@ CREATE TABLE `product_discount` (
 DROP TABLE IF EXISTS `product_review`;
 CREATE TABLE `product_review` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `product_id` INT,
   `user_id` INT,
   `parent_review` INT,
   `content` VARCHAR(255),
@@ -90,11 +90,72 @@ CREATE TABLE `product_review` (
 
 ALTER TABLE `product`
 ADD FOREIGN KEY (`category_id`) REFERENCES `product_category`(`id`),
-ADD FOREIGN KEY (`review_id`) REFERENCES `product_review`(`id`),
 ADD FOREIGN KEY (`inventory_id`) REFERENCES `product_inventory`(`id`),
-ADD FOREIGN KEY (`discount_id`) REFERENCES `product_discount`(`id`),
-ADD FOREIGN KEY (`price_id`) REFERENCES `product_price`(`id`);
+ADD FOREIGN KEY (`discount_id`) REFERENCES `product_discount`(`id`);
+
+ALTER TABLE `product_price`
+ADD FOREIGN KEY (`product_id`) REFERENCES `product`(`id`);
 
 ALTER TABLE `product_review`
-ADD CONSTRAINT `fk_parent_review_id`
-FOREIGN KEY (`parent_review`) REFERENCES `product_review`(`id`);
+ADD CONSTRAINT `fk_parent_review_id` FOREIGN KEY (`parent_review`) REFERENCES `product_review`(`id`),
+ADD CONSTRAINT `fk_product_id_review`FOREIGN KEY (`product_id`) REFERENCES `product`(`id`);
+
+
+INSERT INTO `product` (`code`, `name`, `images`, `height`, `width`, `thickness`, `color`, `surface`, `style`, `application`, `boxsize`, `warranty`, `description`, `tag`, `origin`, `category_id`, `inventory_id`, `discount_id`)
+VALUES
+('P001', 'Wooden Table', 'table_image.jpg', 75.0, 150.0, 5.0, 'Brown', 'Smooth', 'Modern', 'Living Room', 120.0, '1 year', 'A modern wooden table', 'Furniture', 'Vietnam', 1, 1, 1),
+('P002', 'Wooden Chair', 'chair_image.jpg', 90.0, 45.0, 10.0, 'Oak', 'Polished', 'Classic', 'Dining Room', 50.0, '2 years', 'A classic wooden chair', 'Furniture', 'China', 2, 2, 2),
+('P003', 'Wooden Shelf', 'shelf_image.jpg', 180.0, 60.0, 12.0, 'Walnut', 'Patterned', 'Vintage', 'Study Room', 90.0, '3 years', 'A vintage wooden shelf', 'Furniture', 'India', 3, 3, 1),
+('P004', 'Wooden Lamp', 'lamp_image.jpg', 30.0, 20.0, 5.0, 'Maple', 'Glossy', 'Contemporary', 'Living Room', 40.0, '1 year', 'A contemporary wooden lamp', 'Lighting', 'Germany', 4, 4, NULL),
+('P005', 'Wooden Closet', 'closet_image.jpg', 210.0, 180.0, 25.0, 'Cherry', 'Carved', 'Antique', 'Bedroom', 200.0, '2 years', 'An antique wooden closet', 'Furniture', 'France', 1, 5, NULL),
+('P006', 'Wooden Desk', 'desk_image.jpg', 120.0, 80.0, 10.0, 'Pine', 'Textured', 'Rustic', 'Home Office', 100.0, '1.5 years', 'A rustic wooden desk', 'Furniture', 'United States', 2, 6, 3),
+('P007', 'Wooden Bench', 'bench_image.jpg', 150.0, 30.0, 8.0, 'Birch', 'Stained', 'Farmhouse', 'Garden', 80.0, '2 years', 'A stained wooden bench', 'Furniture', 'Canada', 3, 7, 1),
+('P008', 'Wooden Stool', 'stool_image.jpg', 50.0, 50.0, 5.0, 'Cedar', 'Weathered', 'Coastal', 'Patio', 70.0, '1 year', 'A weathered wooden stool', 'Furniture', 'Australia', 4, 8, 2),
+('P009', 'Wooden Mirror', 'mirror_image.jpg', 100.0, 70.0, 7.0, 'Teak', 'Engraved', 'Eclectic', 'Bathroom', 60.0, '2 years', 'An engraved wooden mirror', 'Decor', 'Sweden', 1, 9, NULL),
+('P010', 'Wooden Frame', 'frame_image.jpg', 40.0, 30.0, 3.0, 'Rosewood', 'Antiqued', 'Vintage', 'Living Room', 20.0, '1 year', 'A vintage wooden frame', 'Decor', 'Japan', 2, 10, NULL),
+('P011', 'Wooden Clock', 'clock_image.jpg', 35.0, 35.0, 5.0, 'Ash', 'Handcrafted', 'Minimalist', 'Living Room', 25.0, '1.5 years', 'A handcrafted wooden clock', 'Decor', 'Netherlands', 3, 11, 2),
+('P012', 'Wooden Bed', 'bed_image.jpg', 200.0, 160.0, 20.0, 'Mahogany', 'Carved', 'Traditional', 'Bedroom', 180.0, '4 years', 'A traditional wooden bed', 'Furniture', 'Brazil', 4, 12, 3);
+
+INSERT INTO `product_category` (`code`, `name`)
+VALUES
+('C001', 'Furniture'),
+('C002', 'Decor'),
+('C003', 'Outdoor'),
+('C004', 'Accessories');
+
+
+INSERT INTO `product_price` (`product_id`, `price`, `description`)
+VALUES
+(1, 150.0, 'Price for Wooden Table'),
+(2, 80.0, 'Price for Wooden Chair'),
+(3, 200.0, 'Price for Wooden Shelf'),
+(4, 300.0, 'Price for Wooden Lamp'),
+(5, 400.0, 'Price for Wooden Closet');
+
+
+INSERT INTO `product_inventory` (`quantity`)
+VALUES
+(100),
+(150),
+(200),
+(120),
+(180),
+(250),
+(300),
+(210),
+(280),
+(190),
+(220),
+(175);
+
+INSERT INTO `product_discount` (`name`, `description`, `active`, `discount_percent`)
+VALUES
+('Summer Sale', 'Discount for summer season', 1, 15.0),
+('Festive Discount', 'Discount for festive season', 1, 10.0),
+('Clearance Sale', 'Discount for clearance', 0, 20.0);
+
+
+INSERT INTO `product_review` (`product_id`, `user_id`, `parent_review`, `content`, `like`)
+VALUES
+(1, 1, NULL, 'Great table, sturdy and stylish!', 15),
+(2, 2, NULL, 'Comfortable chair, excellent build.', 12);
